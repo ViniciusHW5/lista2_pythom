833e1528332ddea3a09f73e1906e3e295025d030
ViniciusHW5 <Vinicius_wilbert@estudante.sesisenai.org.br>
2025-02-17 20:36:09 -0300

    Update ExemploFOR_ESTOQUE.py

--- ExemploFOR_ESTOQUE.py
+++ ExemploFOR_ESTOQUE.py
@@ -2,4 +2,5 @@ estoque = [1200,300.800,1500,1900,2750,400,20,23,70,90,80,1100]
 produtos = ['coca cola','pepsi','guarana','skol','brahna','agua','del vale','red bull','dolly','fanta','sprite','sukita','pepsi twist']
 nivel_minimo = 50
 for qtde in estoque:
-    print(qtde)
+    if qtde < nivel_minimo:
+        print(qtde)
