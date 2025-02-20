7b2c2dd898e2a429503e7678649333b2699b5860
ViniciusHW5 <Vinicius_wilbert@estudante.sesisenai.org.br>
2025-02-20 19:45:06 -0300

    dicionario

---
+++ ex004DICIONARIO_remover.py
@@ -0,0 +1,14 @@
+lucro_1tri = {'janeiro': 100000, 'fevereiro': 120000,'março': 90000}
+lucro_2tri = {'abril': 88000,'maio':89000,'junho':120000}
+
+#removendo o mês de junho do dicionário e armazendo em uma variável lucro_jun
+#del lucro_2tri['junho']
+#print(lucro_2tri)
+
+#lucro_jun=lucro_2tri.pop('junho')
+#print(lucro_2tri)
+#print(lucro_jun)
+
+#REMOVENDO TODOS OS DADOS DE UM DICIONARIO:
+lucro_2tri.clear()
+print(lucro_2tri)
