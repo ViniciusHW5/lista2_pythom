549e28f176e7c19b03b24ba6700df77239e86378
ViniciusHW5 <Vinicius_wilbert@estudante.sesisenai.org.br>
2025-02-20 19:25:46 -0300

    Dicionario

---
+++ ex001DICIONARIO..py
@@ -0,0 +1,6 @@
+mais_vendidos = {'tecnologia':'iphone','refrigeracao':'ar consul 1200 btu','livros':'o alquimista','eletrodomesticos':'geladeira'}
+vendas_tecnologia = {'iphone':15000,'samsung galaxy':12000,'Tv samsung':10000,'ps5':14300,'tablet':1720,'ipad':1200}
+livro = mais_vendidos['livros']
+print(mais_vendidos.get('livros'))#usando get para pagar
+print(mais_vendidos['livros'])
+print('Fim programa!')
---
+++ ex002DICIONARIO_ERRO.py
@@ -0,0 +1,9 @@
+mais_vendidos = {'tecnologia':'iphone','refrigeracao':'ar consul 1200 btu','livros':'o alquimista','eletrodomesticos':'geladeira'}
+vendas_tecnologia = {'iphone':15000,'samsung galaxy':12000,'Tv samsung':10000,'ps5':14300,'tablet':1720,'ipad':1200}
+livro = mais_vendidos['livros']
+
+#print(mais_vendidos.get('geladeiras'))
+
+#print(mais_vendidos['geladeiras'])
+
+print('Fim programa!')
