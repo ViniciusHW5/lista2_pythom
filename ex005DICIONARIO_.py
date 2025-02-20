01a139523b61e81886fd0b3086fefb254305e8aa
ViniciusHW5 <Vinicius_wilbert@estudante.sesisenai.org.br>
2025-02-20 19:54:08 -0300

    Update ex005DICIONARIO_.py

--- ex005DICIONARIO_.py
+++ ex005DICIONARIO_.py
@@ -1,5 +1,7 @@
 vendas_tecnologia = {'iphone':15000, 'samsung galaxy': 12000,'tv samsung': 10000, 'ps5': 1430}
 #demonstrando o for 
-for chave in vendas_tecnologia:
+#for chave in vendas_tecnologia:
+for intem in vendas_tecnologia.items():  
     #print(chave)
-    print('O produto{} vendeu {} imodades.'.format(chave,vendas_tecnologia[chave]))
+    #print('O produto{} vendeu {} imodades.'.format(chave,vendas_tecnologia[chave]))
+    print(intem)
