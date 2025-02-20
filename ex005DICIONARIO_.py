fb3a9594e5d6c0c50ca604f1810b8a053901df9e
ViniciusHW5 <Vinicius_wilbert@estudante.sesisenai.org.br>
2025-02-20 19:51:43 -0300

    Create ex005DICIONARIO_.py

---
+++ ex005DICIONARIO_.py
@@ -0,0 +1,5 @@
+vendas_tecnologia = {'iphone':15000, 'samsung galaxy': 12000,'tv samsung': 10000, 'ps5': 1430}
+#demonstrando o for 
+for chave in vendas_tecnologia:
+    #print(chave)
+    print('O produto{} vendeu {} imodades.'.format(chave,vendas_tecnologia[chave]))
